fe0f1f39c49605794441b0ec0bf82a466a9877d5
Ronaldo Vargas <ronaldovh99@gmail.com>
2023-01-26 11:58:37 -0500

    Initial Commit

---
+++ Ejercicio_iOS_Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejercicio_iOS_Storyboard
+//
+//  Created by Ronaldo on 26/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Ejercicio_iOS_StoryboardUITests/Ejercicio_iOS_StoryboardUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Ejercicio_iOS_StoryboardUITestsLaunchTests.swift
+//  Ejercicio_iOS_StoryboardUITests
+//
+//  Created by Ronaldo on 26/01/23.
+//
+
+import XCTest
+
+final class Ejercicio_iOS_StoryboardUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
